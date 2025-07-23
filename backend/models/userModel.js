1c5d65ac0c725f08e831db91d087ae617ae68646
MHDRabeeh <rabirabz001@gmail.com>
2025-07-23 10:38:12 +0530

    add some changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,25 +1,26 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false
-  }
-});
+    username:{
+        type:String,
+        requried:true
+    },
+    email:{
+        type:String,
+        requried:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        requried:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:true,
+        requried:true
+    }
 
-const User = mongoose.model("User", userSchema);
+})
+
+const User = mongoose.model("User",userSchema)
 export default User;
